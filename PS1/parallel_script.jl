09f9d62a149cd7397b38e6c61a81d359e36e7a82
Olivia W <owilkinson@wisc.edu>
2024-09-06 14:55:38 -0500

    example runs

--- PS1/parallel_script.jl
+++ PS1/parallel_script.jl
@@ -1,4 +1,4 @@
-cd("C:/Users/olivi/Documents/Wisconsin/ECON880/PS1")
+
 using Distributed
 addprocs(2)
 @everywhere using Parameters, Plots, SharedArrays #import the libraries we want
@@ -11,17 +11,17 @@ include("parallel_functions.jl") #import the functions that solve our growth mod
 ##############Make plots
 #value function
 plot(k_grid, val_func, title="Value Function V(K)",ylabel = "value V(K)", label = "",xlabel = "capital K")
-savefig("Value_Functions.png")
+savefig("PS1/Graphs/Value_Functions.png")
 
 #policy functions
 plot(k_grid, pol_func, title="Policy Function K'(K)",ylabel = "policy K'(K)", label = "policy K'(K)",xlabel = "capital K",color="blue",linestyle=:solid)
 plot!(k_grid,k_grid,label = "45 degree",color="red",linestyle=:dash)
-savefig("Policy_Functions.png")
+savefig("PS1/Graphs/Policy_Functions.png")
 
 #changes in policy function
 pol_func_δ = pol_func.-k_grid
 plot(k_grid, pol_func_δ, title="Saving Policy Function K'(K) - K",ylabel = "saving policy K'(K) - K", label = "",xlabel = "capital K")
-savefig("Policy_Functions_Changes.png")
+savefig("PS1/Graphs/Policy_Functions_Changes.png")
 
 println("All done!")
 ################################
