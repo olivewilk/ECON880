99ec1ceeaeddfd6132fc1d2477f1c6edc05a9f6d
Olivia W <owilkinson@wisc.edu>
2024-09-17 10:35:25 -0500

    ps2 finished

--- PS2/huggett_model.jl
+++ PS2/huggett_model.jl
@@ -10,25 +10,27 @@
     q_max::Float64 = 1.0 #maximum bond price  
     A_min::Float64 = -2.0 #minimum asset
     A_max::Float64 = 5.0 #maximum asset
-    nA::Int64 = 500 #number of asset grid points
+    nA::Int64 = 1001 #number of asset grid points
     A_grid::Array{Float64,1} = collect(range(A_min, length = nA, stop = A_max))
     Π::Array{Float64,2} = [0.97 0.03; 0.5 0.5] #markov process for earnings 
     q_initial=0.994 #initial bond price
-    μ_dist_initial= vcat(((A_grid.-A_min)./(A_max-A_min)), ((A_grid.-A_min)./(A_max-A_min)))
+    #μ_dist_initial= vcat(((A_grid.-A_min)./(A_max-A_min)), ((A_grid.-A_min)./(A_max-A_min)))
 end
 
 #structure that holds model results
 mutable struct Results
     val_func::Array{Float64, 2} #value function
     pol_func::Array{Float64, 2} #policy function (choice of a')
+    μ_dist::Array{Float64, 1} #cross sectional distribution
 end
 
 #function for initializing model primitives and results
 function Initialize()
     prim = Primitives() #initialize primtiives
     val_func = zeros(prim.nA, prim.ny) #initial value function guess
-    pol_func = zeros(prim.nA, prim.ny ) #initial policy function guess
-    res = Results(val_func, pol_func) #initialize results struct
+    pol_func = zeros(prim.nA, prim.ny ) #initial policy function guess 
+    μ_dist= vcat(((prim.A_grid.- prim.A_min)./(prim.A_max-prim.A_min)), ((prim.A_grid.- prim.A_min)./(prim.A_max-prim.A_min)))
+    res = Results(val_func, pol_func, μ_dist) #initialize results struct
     prim, res #return deliverables
 end
 
@@ -55,8 +57,10 @@ function Bellman(prim::Primitives,res::Results, q::Float64)
     v_next = zeros(nA, ny) #next guess of value function to fill
 
     # Interpolate the value function 
-    v_e = linear_interpolation(A_grid, res.val_func[:,1])
-    v_u = linear_interpolation(A_grid, res.val_func[:,2])
+    #v_e = linear_interpolation(A_grid, res.val_func[:,1])
+    v_e = LinearInterpolation(A_grid, res.val_func[:,1])
+    #v_u = linear_interpolation(A_grid, res.val_func[:,2])
+    v_u = LinearInterpolation(A_grid, res.val_func[:,2])
 
     for y_index = 1:ny
         y = y_grid[y_index]
@@ -90,9 +94,9 @@ function V_iterate(prim::Primitives, res::Results, q::Float64; tol::Float64 = 1e
     println("Value function converged in ", n, " iterations.")
 end
 
-function Tstar(prim::Primitives,res::Results, μ_dist::Array{Float64,1})
+function Tstar(prim::Primitives,res::Results)
     @unpack y_grid, A_grid, A_min, A_max, β, α, nA, ny, Π, q_initial = prim #unpack model primitives
-    @unpack val_func = res #unpack value
+    @unpack val_func, μ_dist = res #unpack value
     trans_mat = zeros(nA*ny,nA*ny) #initialize transition matrix
     for y_index in 1:ny
         for a_index in 1:nA
@@ -112,7 +116,7 @@ function Tstar(prim::Primitives,res::Results, μ_dist::Array{Float64,1})
     # start iteration 
     #it = 1
     #converged = 0
-    tol = 1e-5
+    tol = 1e-10
     max_iter= 1000
     for iter in 1:max_iter
         μ_next = trans_mat' * μ_dist
@@ -126,10 +130,10 @@ end
 
 #Price Solve 
 function Q_Solve(prim::Primitives, res::Results)
-    @unpack val_func = res #unpack value function
-    @unpack y_grid, A_grid, A_min, A_max, β, α, nA, ny, Π, q_initial, μ_dist_initial = prim #unpack model primitives
+    @unpack val_func, μ_dist = res #unpack value function
+    @unpack y_grid, A_grid, A_min, A_max, β, α, nA, ny, Π, q_initial = prim #unpack model primitives
     q = q_initial #initial bond price guess
-    μ_dist = μ_dist_initial #initial distribution guess
+    #μ_dist = μ_dist_initial #initial distribution guess
     #μ_dist = ones(nA*ny)
     mk=0 #market not cleared initialization 
     iterations= 0 
@@ -139,19 +143,19 @@ function Q_Solve(prim::Primitives, res::Results)
 
         V_iterate(prim, res, q) #iterate on value function
         
-        μ_dist=Tstar(prim, res, μ_dist) #iterate on cross sectional distribution
-        μ = reshape(μ_dist, (prim.nA, prim.ny))
-       # println("mu: ",μ_dist)
-        #integral = sum(res.pol_func[:, 1]'*μ_dist[1:nA] + res.pol_func[:, 2]'*μ_dist[nA+1:end])
-        integral = sum(μ.*res.pol_func)
+        res.μ_dist=Tstar(prim, res) #iterate on cross sectional distribution
+        #μ = reshape(res.μ_dist, (prim.nA, prim.ny))
+        #println("mu: ",μ_dist)
+        integral = sum(res.pol_func[:, 1]'*res.μ_dist[1:nA] + res.pol_func[:, 2]'*res.μ_dist[nA+1:end])
+        #integral = sum(μ.*res.pol_func)
         println("summation:",integral)
 
-        if abs(integral)<=.01 #check for market clearing
+        if abs(integral)<=.001 #check for market clearing
             mk = 1 #market cleared
-        elseif integral<-0.01 #adjust bond price
-            q = q-0.00001
-        elseif integral>0.01 #adjust bond price
-            q = q+0.00001
+        elseif integral<-0.001 #adjust bond price
+            q = q-0.01*(1-q)/2
+        elseif integral>0.001 #adjust bond price
+            q = q+0.01*(1-q)/2
         end 
         println("Bond price:",q)
         if iterations>1000 #check for convergence
@@ -161,6 +165,7 @@ function Q_Solve(prim::Primitives, res::Results)
     end 
     mk #return market clearing indicator
     println("Bond price converged in ", iterations, " iterations.")
+    return res.μ_dist
 end 
 
 
--- PS2/huggett_solve.jl
+++ PS2/huggett_solve.jl
@@ -1,12 +1,13 @@
 using Parameters, Plots, LinearAlgebra, Optim, Interpolations #import the libraries we want
+using Trapz
 include("huggett_model.jl") #import the functions that solve our growth model
 
 prim, res = Initialize() #initialize primitive and results structs
 @elapsed @time Solve_model(prim, res) #solve the model!
-@unpack val_func, pol_func = res
-@unpack A_grid = prim
+@unpack val_func, pol_func, μ_dist = res
+@unpack A_grid, nA = prim
 
-#right now my problem is that the transition matrix is all zeros. There must be something wrong with the row and column indexing. 
+#a) Plot the policy function 
 ##############Make plots
 #value function
 Plots.plot(A_grid, val_func[:,1], title="Value Function", label="employed", color="cadetblue", linewidth=:2.0, ylabel="Value V(a)", xlabel="assets")
@@ -18,6 +19,79 @@ Plots.plot(A_grid, pol_func[:,1], title="Policy Functions", label="employed", co
 plot!(A_grid, pol_func[:,2], label="unemployed", color="palevioletred", linewidth=:2.0)
 plot!(A_grid,A_grid,label = "45 degree",color="gray40",linestyle=:dash)
 Plots.savefig("PS2/Graphs/Policy_Functions.png")
+
+#b) plot the cross sectional distribution of wealth for employed and unemployed agents
+Plots.bar(A_grid, μ_dist[1:nA], title="Cross Sectional Distribution of Wealth", label="employed", color="cadetblue",linecolor="cadetblue", ylabel="Fraction of population", xlabel="assets")
+bar!(A_grid, μ_dist[nA+1:end], label="unemployed", color="palevioletred", linecolor="palevioletred")
+Plots.savefig("PS2/Graphs/Wealth_crosssectional_dist.png")
+
+#Lorenz curve 
+#fraction of wealth = y and fraction of population = x 
+
+total_fraction = zeros(nA)
+for i in 1:nA
+    local x = μ_dist[i]
+    local y = μ_dist[i+nA]
+    total_fraction[i] = x+y
+end 
+population_lorenz = cumsum(total_fraction)
+
+earnings_e = A_grid.+prim.y_grid[1]
+earnings_u = A_grid.+prim.y_grid[2]
+earnings = zeros(nA)
+for i in 1:nA
+    local x = μ_dist[i]*earnings_e[i]
+    local y = μ_dist[i+nA]*earnings_u[i]
+    earnings[i]= x+y
+end 
+total_wealth = sum(earnings)
+wealth_lorenz = cumsum(earnings)/total_wealth
+
+Plots.plot(population_lorenz, wealth_lorenz, title="Lorenz Curve", label="Lorenz Curve", color="cadetblue", linewidth=:2.0, ylabel="Fraction of wealth", xlabel="Fraction of population")
+plot!(population_lorenz, population_lorenz, label="45 degree", color="gray40", linewidth=:2.0, linestyle=:dash)
+Plots.savefig("PS2/Graphs/Lorenz.png")
+
+#Calculate Gini coefficient
+area = trapz(population_lorenz,wealth_lorenz)
+@show gini = 1 - 2 * area
+println("Gini coefficient in US 2021 is .398 (FRED). Our model has a Gini coefficient of $gini")
+#gini should be .3821
+
+## PART III Welfare Anaylsis
+w_fb = (((.9715)^(1-prim.α)-1)/(1-prim.α))/(1-prim.β)
+αβ_frac = 1/((1-prim.α)*(1-prim.β))
+num= w_fb + αβ_frac
+denom = val_func .+ αβ_frac
+lambda = (num./denom).^(1/(1-prim.α)) .-1
+
+#a) plot lamba 
+Plots.plot(A_grid, lambda[:,1], title="Consumption Equivalent", label="λ(a,e) (employed)", color="cadetblue", linewidth=:2.0, ylabel="λ(a,s)", xlabel="assets")
+plot!(A_grid, lambda[:,2], label="λ(a,u) (unemployed)", color="palevioletred", linewidth=:2.0)
+Plots.savefig("PS2/Graphs/Consumption_Equivalent.png")
+
+#b) calculate the welfare first best and welfare incomplete markets and welfare gain 
+μ = reshape(μ_dist, (prim.nA, prim.ny))
+w_inc = sum(μ.*val_func)
+w_gain = sum(μ.*lambda)
+println("Welfare first best is $w_fb")
+println("Welfare incomplete markets is $w_inc")
+println("Welfare gain is $w_gain")
+
+vote = zeros(prim.nA, prim.ny)
+for i in 1:prim.nA
+    for j in 1:prim.ny
+        if lambda[i,j] > 0
+            vote[i,j] = 1
+        else
+            vote[i,j] = 0
+        end
+    end
+end 
+
+#c) what fraction would vote for complete markets?
+fraction_change = sum(μ.*vote)
+println("Fraction of agents that would pay for complete markets is $fraction_change")
+
 
 println("All done!")
 ################################
